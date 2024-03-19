3ac557f9c0c75228176d1a8363c99bf0f024d081
luizricci <luizaureliano.tds1.senai@gmail.com>
2024-03-19 10:29:07 -0300

    correção de bug

--- atividade.js
+++ atividade.js
@@ -1,12 +1,12 @@
-let texto = "micro empreendedor";
+let texto = "macro";
 
 let textoMinusculo = texto.toLowerCase();
 console.log("texto: " + textoMinusculo);
 
-let separar = texto.split("");
+let separar = textoMinusculo.split("");
 
 let vogais = separar.filter((separar) => ["a", "e", "i", "o", "u"].includes(separar));
 let ordem = vogais.sort()
 
 console.log("vogais encontradas: " + vogais)
-console.log("numero total de vogais: " + vogais.length++)   
+console.log("numero total de vogais: " + vogais.length++)
